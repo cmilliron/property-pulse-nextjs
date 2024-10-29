2e12afa4978ff81ab4dbb7462a0dd5584a6d439b
Cody Milliron <codymilliron.dev@gmail.com>
2024-10-28 22:04:53 -0400

    finish layout and homepage base configureations

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,18 @@
-const MainLayout = () => {
+import "@/assets/styles/globals.css";
+
+export const metadata = {
+  title: "PropertyPulse",
+  description: "Find The Pefect Rental Property",
+};
+
+function MainLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <main>MainLayout</main>
+        <main>{children}</main>
       </body>
     </html>
   );
-};
+}
 
 export default MainLayout;
---
+++ src/app/page.jsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <div className="text-2xl">Home Page</div>;
+}
+
+export default HomePage;
