230eef51ac64741d63d58b5d0b63be3f568ed5a9
Cody Milliron <codymilliron.dev@gmail.com>
2024-10-29 23:19:08 -0400

    add footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import Footer from "@/components/Footer";
 
 function HomePage() {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <Footer />
     </>
   );
 }
