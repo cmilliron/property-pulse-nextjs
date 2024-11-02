3d2f7a3810111fe78effdf874f8331761c8d23bd
Cody Milliron <codymilliron.dev@gmail.com>
2024-11-01 22:24:05 -0400

    add call to get properties from databose

--- src/app/api/properties/route.js
+++ src/app/api/properties/route.js
@@ -4,8 +4,9 @@ export const GET = async (request) => {
   try {
     await connectDb();
 
+    const properties = await Property.find({});
     return new Response(
-      JSON.stringify({ result: "success", message: "Hello World" })
+      JSON.stringify(JSON.stringify(properties, { status: 200 }))
     );
   } catch (error) {
     return new Response("Something went wrong", { status: 500 });
