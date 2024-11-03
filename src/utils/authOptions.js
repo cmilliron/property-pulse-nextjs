9850b856c7eba9f1c5ab4aab85492f4b0e183545
Cody Milliron <codymilliron.dev@gmail.com>
2024-11-03 07:59:47 -0500

    Add login functionality to NavBar component using NextAuth"

--- src/utils/authOptions.js
+++ src/utils/authOptions.js
@@ -14,4 +14,19 @@ export const authOptions = {
       },
     }),
   ],
+  callbacks: {
+    // Invoked on successful signin
+    async signIn({ profile }) {
+      // 1. Connect ot the database
+      // 2. check if user exists
+      // 3. if not, create user
+      // 4. Retunr ture to allow sign in
+    },
+    // Session callback funcation that momdifies the session object
+    async session({ session }) {
+      // 1. Get user from database
+      // 2. Assign user id from the session
+      // 3. Return session
+    },
+  },
 };
