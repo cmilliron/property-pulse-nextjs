689d962810c6782bda6f7abd28bb86c0dd808613
Cody Milliron <codymilliron.dev@gmail.com>
2024-11-09 19:11:21 -0500

    removed fuctionality of Geocode functionality

--- src/components/PropertyMaps.jsx
+++ src/components/PropertyMaps.jsx
@@ -1,83 +1,83 @@
-"use client";
-import { useEffect, useState } from "react";
-import { setDefaults, fromAddress } from "react-geocode";
-import Map, { Marker } from "react-map-gl";
-import Image from "next/image";
-import pin from "@/assets/images/pin.svg";
-import Spinner from "./Spinner";
-import "mapbox-gl/dist/mapbox-gl.css";
+// "use client";
+// import { useEffect, useState } from "react";
+// // import { setDefaults, fromAddress } from "react-geocode";
+// import Map, { Marker } from "react-map-gl";
+// import Image from "next/image";
+// import pin from "@/assets/images/pin.svg";
+// import Spinner from "./Spinner";
+// import "mapbox-gl/dist/mapbox-gl.css";
 
-function PropertyMaps({ property }) {
-  const [lat, setLat] = useState(null);
-  const [lng, setLng] = useState(null);
-  const [viewport, setViewport] = useState({
-    latitude: 0,
-    longitude: 0,
-    zoom: 12,
-    width: "100%",
-    height: "500px",
-  });
-  const [loading, setLoading] = useState(true);
-  const [geocodeError, setGeocodeError] = useState(false);
-  setDefaults({
-    key: process.env.NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY,
-    language: "en",
-    region: "us",
-  });
+// function PropertyMaps({ property }) {
+//   const [lat, setLat] = useState(null);
+//   const [lng, setLng] = useState(null);
+//   const [viewport, setViewport] = useState({
+//     latitude: 0,
+//     longitude: 0,
+//     zoom: 12,
+//     width: "100%",
+//     height: "500px",
+//   });
+//   const [loading, setLoading] = useState(true);
+//   const [geocodeError, setGeocodeError] = useState(false);
+//   setDefaults({
+//     // key: process.env.NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY,
+//     language: "en",
+//     region: "us",
+//   });
 
-  useEffect(() => {
-    const fetchCoords = async () => {
-      try {
-        const { location } = property;
-        const address = `${location.address}, ${location.city}, ${location.state} ${location.zipcode}`;
-        const res = await fromAddress(address);
+//   // useEffect(() => {
+//   //   const fetchCoords = async () => {
+//   //     try {
+//   //       const { location } = property;
+//   //       const address = `${location.address}, ${location.city}, ${location.state} ${location.zipcode}`;
+//   //       const res = await fromAddress(address);
 
-        if (res.results.length === 0) {
-          setGeocodeError(true);
-          return;
-        }
+//   //       if (res.results.length === 0) {
+//   //         setGeocodeError(true);
+//   //         return;
+//   //       }
 
-        const { lat, lng } = res.results[0].geometry.location;
-        setLat(lat);
-        setLng(lng);
-        setViewport((prevState) => ({
-          ...prevState,
-          latitude: lat,
-          longitude: lng,
-        }));
-      } catch (error) {
-        console.log(error);
-        setGeocodeError(true);
-      } finally {
-        setLoading(false);
-      }
-    };
+//   //       const { lat, lng } = res.results[0].geometry.location;
+//   //       setLat(lat);
+//   //       setLng(lng);
+//   //       setViewport((prevState) => ({
+//   //         ...prevState,
+//   //         latitude: lat,
+//   //         longitude: lng,
+//   //       }));
+//   //     } catch (error) {
+//   //       console.log(error);
+//   //       setGeocodeError(true);
+//   //     } finally {
+//   //       setLoading(false);
+//   //     }
+//   //   };
 
-    fetchCoords();
-  });
+//   //   fetchCoords();
+//   // }, []);
 
-  if (loading) return <Spinner />;
+//   if (loading) return <Spinner />;
 
-  if (geocodeError)
-    return <div className="text-xl">No loacation data found</div>;
+//   if (geocodeError)
+//     return <div className="text-xl">No loacation data found</div>;
 
-  return (
-    <Map
-      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
-      mapLib={import("mapbox-gl")}
-      initialViewState={{
-        longitude: lng,
-        latitude: lat,
-        zoom: 15,
-      }}
-      style={{ width: "100%", height: 500 }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    >
-      <Marker longitude={lng} latitude={lat} anchor="bottom">
-        <Image src={pin} alt={location} width={40} height={40} />
-      </Marker>
-    </Map>
-  );
-}
+//   return (
+//     <Map
+//       mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+//       mapLib={import("mapbox-gl")}
+//       initialViewState={{
+//         longitude: lng,
+//         latitude: lat,
+//         zoom: 15,
+//       }}
+//       style={{ width: "100%", height: 500 }}
+//       mapStyle="mapbox://styles/mapbox/streets-v9"
+//     >
+//       <Marker longitude={lng} latitude={lat} anchor="bottom">
+//         <Image src={pin} alt={location} width={40} height={40} />
+//       </Marker>
+//     </Map>
+//   );
+// }
 
-export default PropertyMaps;
+// export default PropertyMaps;
